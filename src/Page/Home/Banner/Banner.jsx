6ac220e306b485ca799ca819bd72d3e87e82da82
AnikHassan111 <miaanik4@gmail.com>
2023-11-08 21:50:46 +0600

    add discout banner section img

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,7 +1,21 @@
 const Banner = () => {
   return (
     <div>
-      <h1>Banner</h1>
+      <div>
+        <h1>Exclusive Hotel Discounts for Your Next Getaway</h1>
+        <p>
+          Are you dreaming of a memorable vacation but worried about your
+          budget? Look no further! We are excited to offer you exclusive hotel
+          discounts that will make your next getaway even more affordable and
+          enjoyable
+        </p>
+      </div>
+      <div>
+        <img
+          src="https://i.ibb.co/QmCXNy0/francesca-saraco-d-S27-XGg-Ry-Q-unsplash.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
