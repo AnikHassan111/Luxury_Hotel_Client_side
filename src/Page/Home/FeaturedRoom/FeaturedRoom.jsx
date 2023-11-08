20d7e819078a926a57ffb778945c402e128445b5
AnikHassan111 <miaanik4@gmail.com>
2023-11-08 22:18:54 +0600

    get featrued data

--- src/Page/Home/FeaturedRoom/FeaturedRoom.jsx
+++ src/Page/Home/FeaturedRoom/FeaturedRoom.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const FeaturedRoom = () => {
+  const [featruedData, setFeatruedData] = useState([]);
+  useEffect(() => {
+    axios("http://localhost:5000/featuredroom").then((res) =>
+      setFeatruedData(res.data)
+    );
+  }, []);
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Featured Section</h1>
