a1dece9734c6dc0c048f627743cf353f9d08337f
AnikHassan111 <miaanik4@gmail.com>
2023-11-08 21:05:07 +0600

    add mybooking navigation function

--- src/Page/Room/Room.jsx
+++ src/Page/Room/Room.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import RoomCard from "./RoomCard";
 import { object } from "prop-types";
+import { Link } from "react-router-dom";
 
 const Room = () => {
   const [roomData, setRoomData] = useState([]);
@@ -14,12 +15,19 @@ const Room = () => {
   }, []);
 
   return (
-    <div className="max-w-7xl mx-auto gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {roomData.map((data) => (
-        <RoomCard key={data._id} data={data}>
-          {" "}
-        </RoomCard>
-      ))}
+    <div>
+      <Link to={"/mybooking"}>
+        <h1 className="text-center font-bold text-3xl my-3 cursor-pointer">
+          My Booking
+        </h1>
+      </Link>
+      <div className="max-w-7xl mx-auto gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {roomData.map((data) => (
+          <RoomCard key={data._id} data={data}>
+            {" "}
+          </RoomCard>
+        ))}
+      </div>
     </div>
   );
 };
